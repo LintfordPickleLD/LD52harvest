809dc721c60b5c395f606dfd92df6335c6e1f8ec
LintfordPickle <jonnohampson@gmail.com>
2025-12-16 22:04:52 +0100

    more work on assets

--- raw/build spritesheets.bat
+++ raw/build spritesheets.bat
@@ -3,10 +3,12 @@
 Call "D:\dev\tools\spritepacker\SpritePacker.exe" "D:\dev\java\ludumdare\52 - Harvest\projects\LD52_Harvest\raw\spritesheets\spritesheet_core_o.lss"
 Call "D:\dev\tools\spritepacker\SpritePacker.exe" "D:\dev\java\ludumdare\52 - Harvest\projects\LD52_Harvest\raw\spritesheets\spritesheet_hud_o.lss"
 
-Call "D:\dev\tools\spritepacker\SpritePacker.exe" "D:\dev\java\ludumdare\52 - Harvest\projects\LD52_Harvest\raw\spritesheets\spritesheet_props.lss"
+Call "D:\dev\tools\spritepacker\SpritePacker.exe" "D:\dev\java\ludumdare\52 - Harvest\projects\LD52_Harvest\raw\spritesheets\spritesheet_spritesui.lss"
+Call "D:\dev\tools\spritepacker\SpritePacker.exe" "D:\dev\java\ludumdare\52 - Harvest\projects\LD52_Harvest\raw\spritesheets\spritesheet_spritesmenu.lss"
+Call "D:\dev\tools\spritepacker\SpritePacker.exe" "D:\dev\java\ludumdare\52 - Harvest\projects\LD52_Harvest\raw\spritesheets\spritesheet_spritesgame.lss"
 Call "D:\dev\tools\spritepacker\SpritePacker.exe" "D:\dev\java\ludumdare\52 - Harvest\projects\LD52_Harvest\raw\spritesheets\spritesheet_scene.lss"
 
 
-Call "D:\dev\tools\spritepacker\SpritePacker.exe" "D:\dev\java\ludumdare\52 - Harvest\projects\LD52_Harvest\raw\spritesheets\spritesheet_AdWallSoup.lss"
+Call "D:\dev\tools\spritepacker\SpritePacker.exe" "D:\dev\java\ludumdare\52 - Harvest\projects\LD52_Harvest\raw\spritesheets\spritesheet_adwallsoup.lss"
 
 pause
